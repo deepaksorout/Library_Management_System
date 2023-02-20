413f3c409ba2cc6fa5df211571fa1260cb5728dd
deepaksorout <39903636+deepaksorout@users.noreply.github.com>
2023-02-20 15:22:58 +0530

    message

--- src/main/java/Student_Library/Library_Management/Controllers/AuthorController.java
+++ src/main/java/Student_Library/Library_Management/Controllers/AuthorController.java
@@ -1,6 +1,7 @@
 package Student_Library.Library_Management.Controllers;
 
 import Student_Library.Library_Management.DTOs.AuthorEntityDto;
+import Student_Library.Library_Management.DTOs.AuthorResponseDto;
 import Student_Library.Library_Management.Models.Author;
 import Student_Library.Library_Management.Service.AuthorService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,5 +16,9 @@ public class AuthorController {
     @PostMapping("/add")
     public String createAuthor(@RequestBody AuthorEntityDto authorEntityDto){
         return authorService.createAuthor(authorEntityDto);
+    }
+    @GetMapping("/getAuthor")
+    public AuthorResponseDto getAuthorById(@RequestParam("authorId") int authorId){
+        return authorService.getAuthorById(authorId);
     }
 }
